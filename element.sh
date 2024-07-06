b2025da3c9bd0ca6744879b3b396008279279efb
Uruc Tarim <i.uructarim@gmail.com>
2024-07-06 06:30:25 +0000

    feat: Implement element query and output in element.sh

--- element.sh
+++ element.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-# Check if an argument is provided
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [[ $1 ]]
 then
-  echo "Script is not yet implemented"
+  # Check if the argument is a number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+  else
+    # Check if the argument is a symbol or name
+    ELEMENT=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+  fi
+
+  if [[ -z $ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    echo $ELEMENT | while IFS="|" read TYPE_ID ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
+  fi
 else
   echo "Please provide an element as an argument."
 fi
